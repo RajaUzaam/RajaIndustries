c2616c821b362177e45be768ecbbadd1c84179bd
UzaamAhmad <ruzaam7@gmail.com>
2025-06-30 20:52:54 +0500

    NavBar Changed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
 import Link from 'next/link'
 import { Heart, ShoppingBag, UserRound, Search } from 'lucide-react'
 import { Montserrat } from "next/font/google";
-
+import  Image  from 'next/image';
+import React, { Component } from 'react';
+//import '../styles/index.css'
 const montserrat = Montserrat({
     variable: "--font-montserrat",
     subsets: ["latin"]
@@ -22,8 +24,8 @@ const NavLink = ({ children, href }: {children: string, href: string}) => {
 
 const SearchBar = () => {
     return (
-        <div className="flex items-center w-full max-w-md px-1 py-1 bg-gray-50 hover:bg-gray-100 rounded-[7px] shadow-sm focus-within:ring-2 focus-within:ring-black transition">
-        <button className='mr-1.5 border-gray-300 border-2 rounded-[7px] p-1 hover:cursor-pointer hover:border-gray-800 transition'>
+        <div className="flex items-center w-[20rem] px-1 py-1 bg-gray-50 hover:bg-gray-100 rounded-[7px] shadow-sm focus-within:ring-2 focus-within:ring-black transition">
+        <button className='mx-1.5 border-gray-300 border-2 rounded-[7px] p-1 hover:cursor-pointer hover:border-gray-800 transition'>
         <Search className="w-[1.25rem] h-[1.25rem]" />
         </button>
         <input
@@ -35,32 +37,60 @@ const SearchBar = () => {
     )
 }
 
+const NavIcon = ({
+  href,
+  ariaLabel,
+  children,
+}: {
+  href: string;
+  ariaLabel: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <Link
+      href={href}
+      aria-label={ariaLabel}
+      className='
+      inline-block p-1.5 rounded-[7px] 
+      border border-gray-300 bg-gray-100 shadow-sm hover:bg-gray-900
+      hover:text-white hover:scale-110 transition-all duration-300
+      '
+    >
+    {children}
+    </Link>
+  );
+};
+
+
 const Header = () => {
   return (
-    <header className="w-full px-6 py-4 bg-white shadow-md mx-auto flex items-center justify-between gap-6">
+    <header className="w-full px-6 py-4 bg-white shadow-md flex">
       {/* Logo */}
       <Link href="/" className={`text-[1.5rem] font-bold ${montserrat.className} pt-1`}>
-        Raja International
+        <Image src={`/logo.svg`} alt={`R LOGO`} width={90} height={90}/>
       </Link>
 
       {/* Navigation Links */}
-      <nav className="flex gap-8 justify-center flex-1 text-md">
+      <nav className="flex w-full my-auto gap-6 mx-4 text-md">
         <NavLink href='/'>Home</NavLink>
         <NavLink href="/">Shop</NavLink>
+        <NavLink href="/">Men</NavLink>
+        <NavLink href="/">Women</NavLink>
+        <NavLink href="/">Kids</NavLink>
       </nav>
 
       {/* Icons (cart, wishlist, user) */}
-      <div className="flex items-center gap-5">
+      <div className="flex items-center my-auto gap-5">
         <SearchBar />
-        <Link href="/wishlist" aria-label="Wishlist">
+        <NavIcon href='/wishlist' ariaLabel='Wishlist'>
           <Heart />
-        </Link>
-        <Link href="/cart" aria-label="Cart">
+        </NavIcon>
+        <NavIcon href="/cart" ariaLabel="Cart">
           <ShoppingBag />
-        </Link>
-        <Link href="/account" aria-label="Account">
+        </NavIcon>
+        <NavIcon href="/account" ariaLabel="Account">
           <UserRound />
-        </Link>
+        </NavIcon>
       </div>
     </header>
   )
